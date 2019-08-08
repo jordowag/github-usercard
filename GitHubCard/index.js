06a086692bdc01c150a5170a97641223b795b0ca
Jordan <jordandoan@hotmail.com>
2019-08-08 12:42:17 -0700

    Successfully did a GET request, and used data from promise to make user card and appended to webpage

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,7 @@
            https://api.github.com/users/<your name>
 */
 
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -24,7 +25,7 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -45,6 +46,56 @@ const followersArray = [];
 </div>
 
 */
+
+const url = "https://api.github.com/users/";
+const friendsArray = ["jordowag", "tetondan","dustinmyers", "justsml", "luishrd", "bigknell"]
+friendsArray.forEach((friend) => {
+  axios.get(`${url}${friend}`)
+    .then(createUserCard)
+    .then(appendToPage)
+    .catch((err) => {
+      console.log(err);
+    });
+});
+function createUserCard(response) {
+  // Create Elements and Add Classes
+  console.log(response);
+  let data = response.data;
+  let card = document.createElement("div");
+  card.classList.add("card");
+  let avatar = document.createElement("img");
+  let info = document.createElement("div");
+  info.classList.add("card-info");
+  let name = document.createElement("h3");
+  name.classList.add("name");
+  let username = document.createElement("p");
+  username.classList.add("username");
+  let location = document.createElement("p");
+  let profile = document.createElement("p");
+  let pageURL = document.createElement("a");
+  let followerCount = document.createElement("p");
+  let followingCount = document.createElement("p");
+  let biography = document.createElement("p");
+  // Create Structure
+  card.append(avatar,info);
+  info.append(name,username,location,profile,followerCount,followingCount,biography);
+  profile.append(pageURL);
+  // Add text content
+  avatar.src = data.avatar_url;
+  name.textContent = data.name;
+  username.textContent = data.login;
+  location.textContent = data.location;
+  pageURL.href = data.html_url;
+  pageURL.textContent = data.html_url;
+  followerCount.textContent = data.followers;
+  followingCount.textContent = data.following;
+  return card;
+}
+
+function appendToPage(user){
+  let cards = document.querySelector(".cards");
+  cards.append(user);
+}
 
 /* List of LS Instructors Github username's: 
   tetondan
